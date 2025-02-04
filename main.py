0b0e42865bd2fc0812c2d064d42f5206e0c97feb
ankuraxz <34418414+Ankuraxz@users.noreply.github.com>
2025-02-04 16:37:15 -0500

    no venv

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 #basic Hello world fast api
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+app = FastAPI(
+    title="GDSC SAMPLE",
+    description="APIs for GDSC",
+    version="1.0.0",
+    openapi_url="/openapi.json",
+    docs_url="/"
+)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/hello")
 async def root():
     return {"message": "Hello World"}
